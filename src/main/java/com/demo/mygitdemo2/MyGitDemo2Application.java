7c37eea9fb66fcc408fe7a483502e276de151fb5
hp-sr <xuedengshan@qq.com>
2019-05-26 23:04:44 +0800

    sss

---
+++ src/main/java/com/demo/mygitdemo2/MyGitDemo2Application.java
@@ -0,0 +1,13 @@
+package com.demo.mygitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mygitdemo2/MyGitDemo2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.mygitdemo2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyGitDemo2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
